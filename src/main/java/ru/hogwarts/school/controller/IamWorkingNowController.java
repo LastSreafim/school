21e415fcd9accb8bb279ad563d91448dc9ccb44b
Nazar <epoxasmute@mail.ru>
2024-10-09 20:29:21 +0300

    Check point

--- src/main/java/ru/hogwarts/school/controller/IamWorkingNowController.java
+++ src/main/java/ru/hogwarts/school/controller/IamWorkingNowController.java
@@ -1,12 +1,14 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("info")
 @RestController
 public class IamWorkingNowController {
     @GetMapping
-    public String testApi() {
-        return "HI! NOW I AM WORKING";
+    public ResponseEntity<String> testApi() {
+        return ResponseEntity.ok("HI! NOW I AM WORKING") ;
     }
 }
--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,46 @@
 package ru.hogwarts.school;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.school.controller.AvatarController;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Student;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private FacultyController facultyController;
+
+    @Autowired
+    private AvatarController avatarController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() throws Exception {
+        Assertions.assertThat(studentController).isNotNull();
+        Assertions.assertThat(facultyController).isNotNull();
+        Assertions.assertThat(avatarController).isNotNull();
+    }
+
+    @Test
+    public void testInfo() throws Exception {
+        Assertions.assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/info", String.class))
+                .isEqualTo("HI! NOW I AM WORKING");
+    }
+
 
 }
